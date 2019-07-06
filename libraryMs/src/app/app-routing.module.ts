a9d91aca5eb8fff4d6475be23d28047e5f58fd4a
S.M.Kashif <kashi_rock82@yahoo.com>
2019-07-06 12:22:28 +0500

    set routes

--- libraryMs/src/app/app-routing.module.ts
+++ libraryMs/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookdetailsComponent } from './bookdetails/bookdetails.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent},
+	{ path: 'book-list', component: BooklistComponent},
+	{ path: 'book-details', component: BookdetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
