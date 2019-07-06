7f44755e6858f0de74d867a7426e030a41c6a55b
S.M.Kashif <kashi_rock82@yahoo.com>
2019-07-06 11:58:32 +0500

    make components home, booklist, bookdetails

--- libraryMs/src/app/app.module.ts
+++ libraryMs/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookdetailsComponent } from './bookdetails/bookdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooklistComponent,
+    BookdetailsComponent
   ],
   imports: [
     BrowserModule,
