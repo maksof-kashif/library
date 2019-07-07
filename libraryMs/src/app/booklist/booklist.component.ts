7a9f375ba365297465b92825b01679912dfd1360
S.M.Kashif <kashi_rock82@yahoo.com>
2019-07-07 13:47:47 +0500

    add pagination

--- libraryMs/src/app/booklist/booklist.component.ts
+++ libraryMs/src/app/booklist/booklist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationsService } from 'angular2-notifications';
-import * as books from '../tempdb/books.json';
+import { CommonService } from '../common.service';
+import * as books from "../tempdb/books.json";
 
 @Component({
   selector: 'app-booklist',
@@ -9,14 +10,60 @@ import * as books from '../tempdb/books.json';
 })
 export class BooklistComponent implements OnInit {
 
-  constructor(public notificationService: NotificationsService) { }
+  constructor(public notificationService: NotificationsService, private commonService: CommonService) { }
+  
+  	book = [];
+  	tabledata = [];
+  	offset = 0;
+	limit = 2;
+	private totalItems: any[];
+	pager: any = {};
+	pagedItems: any[];
+	start = true;
+
 
   ngOnInit() {
-  	console.log(books);
+  	this.getBookData();
   }
 
 
   backBtn(){
   	window.history.back();
   }
+
+  getBookData(){
+  	this.book = books.default.booksDetails;
+  	this.totalItems = books.default.booksDetails.length;
+  	if (this.start == true)this.setPage(1);
+  }
+
+  setPage(page: number) {
+	this.start = false;
+    this.pager = this.commonService.getPager(this.totalItems, page, this.limit);
+	if(page > this.pager.endPage) page = this.pager.endPage;
+	if(page < 1) page = 1;
+    this.offset = (page - 1) * this.limit;
+    this.tabledata = this.setDataForPage();
+    console.log(this.tabledata);
+	}
+
+	setDataForPage(){
+		var displayData = [];
+		if(this.offset == 0){
+			var start = this.offset;
+			var end = this.limit+this.offset;
+		} else{
+			var start = this.offset;
+			var end = this.limit+this.offset;
+		}
+
+		for(var i = start; i < end; i++){
+			if(this.book[i] == undefined){
+				break;
+			} else{
+				displayData.push(this.book[i]);
+			}
+		}
+		return displayData;
+	}
 }
